6573e63753efb5b3fa5234917e6fa6dd78dbec25
Stephen Fuqua <stephen.fuqua@trintech.com>
2015-12-20 23:19:39 -0600

    Create authorized navigation tree

---
+++ src/Navigation/NavController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Web.Http;
+using FligthNode.Common.Api.Controllers;
+using System.Linq;
+
+namespace FlightNode.Service.Navigation
+{
+    public class NavController : LoggingController
+    {
+        public IHttpActionResult Get()
+        {
+            var reqContext = this.RequestContext;
+
+            var parent = new NavigationNode();
+            parent.AddChild(new NavigationNode("Login", "#/login"));
+
+            var claimsPrincipal = reqContext.Principal as ClaimsPrincipal;
+            if (claimsPrincipal == null)
+            {
+                return Ok(parent);
+            }
+
+            var claims = (claimsPrincipal.Claims ?? new List<Claim>()).ToList();
+            if (claims.Any(x => HasRole(x, "Administrator")))
+            {
+                parent = AddAdministrativeTree(parent);
+            }
+            if (claims.Any(x => HasRole(x, "Reporter")))
+            {
+                parent = AddReporterTree(parent);
+            }
+
+            return Ok(parent);
+        }
+
+        private static bool HasRole(Claim x, string roleName)
+        {
+            return x.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role" && 
+                   x.Value == roleName;
+        }
+
+        private NavigationNode AddAdministrativeTree(NavigationNode parent)
+        {
+            parent = BuildUserMenu(parent);
+
+
+
+            return parent;
+        }
+
+        private static NavigationNode BuildUserMenu(NavigationNode parent)
+        {
+            var users = new NavigationNode("Manage", "#/users");
+
+            users.AddChild(new NavigationNode("Users", "#/users"));
+            users.AddChild(new NavigationNode("Work Types", "#/worktypes"));
+            users.AddChild(new NavigationNode("Locations", "#/locations"));
+
+            parent.AddChild(users);
+            return parent;
+        }
+
+        private NavigationNode AddReporterTree(NavigationNode parent)
+        {
+            var collection = new NavigationNode("Reporting", "");
+
+            collection.AddChild(new NavigationNode("Work Log", "#/workday/create"));
+
+            return parent;
+        }
+    }
+}
---
+++ src/Navigation/NavigationNode.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlightNode.Service.Navigation
+{
+    public class NavigationNode
+    {
+        public List<NavigationNode> Children { get; private set; }
+
+        public NavigationEntry Entry { get; set; }
+
+        public NavigationNode()
+        {
+            Children = new List<NavigationNode>();
+        }
+
+        public NavigationNode(string title, string path)
+        {
+            Children = new List<NavigationNode>();
+            Entry = new NavigationEntry
+            {
+                Title = title,
+                Path = path
+            };
+        }
+
+        public NavigationNode AddChild(NavigationNode child)
+        {
+            Children.Add(child);
+            return this;
+        }
+
+    }
+
+    public class NavigationEntry
+    {
+        public string Title { get; set; }
+        public string Path { get; set; }
+    }
+}
