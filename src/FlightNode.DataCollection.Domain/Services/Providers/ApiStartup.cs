cd6dee279427a8354c143019ebb69a3087664008
Stephen Fuqua <stephen@safnet.com>
2016-01-11 22:51:26 -0600

    Tweak the startup process for IIS support.

--- src/FlightNode.DataCollection.Domain/Services/Providers/ApiStartup.cs
+++ src/FlightNode.DataCollection.Domain/Services/Providers/ApiStartup.cs
@@ -8,56 +8,11 @@ namespace FlightNode.DataCollection.Services.Providers
     {
         public static IAppBuilder Configure(IAppBuilder app, string issuer)
         {
-            app = ConfigureIdentityManagement(app, issuer);
-            app = ConfigureOAuthTokenConsumption(app, issuer);
 
             return app;
         }
 
-        private static IAppBuilder ConfigureIdentityManagement(IAppBuilder app, string issuer)
-        {
-            //app.CreatePerOwinContext(IdentityDbContext.Create);
-            //app.CreatePerOwinContext<UserManager>(UserManager.Create);
-
-            //var OAuthServerOptions = new OAuthAuthorizationServerOptions()
-            //{
-            //    //For Dev enviroment only (on production should be AllowInsecureHttpConnection = false)
-            //    AllowInsecureHttp = Properties.Settings.Default.AllowInsecureHttpConnection,
-            //    TokenEndpointPath = new PathString("/oauth/token"),
-            //    AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
-            //    Provider = new OAuthProvider(),
-            //    ApplicationCanDisplayErrors = false,
-
-            //    AccessTokenFormat = new JwtFormat(issuer)
-            //};
-
-            //// OAuth 2.0 Bearer Access Token Generation
-            //app.UseOAuthAuthorizationServer(OAuthServerOptions);
-
-            return app;
-        }
-
-        private static IAppBuilder ConfigureOAuthTokenConsumption(IAppBuilder app, string issuer)
-        {
-            //var audienceId = Properties.Settings.Default.AudienceId;
-            //var audienceSecret = TextEncodings.Base64Url.Decode(Properties.Settings.Default.AudienceSecret);
-
-            //// Api controllers with an [Authorize] attribute will be validated with JWT
-            //app.UseJwtBearerAuthentication(
-            //    new JwtBearerAuthenticationOptions
-            //    {
-            //        AuthenticationMode = AuthenticationMode.Active,
-            //        AllowedAudiences = new[] { audienceId },
-            //        IssuerSecurityTokenProviders = new IIssuerSecurityTokenProvider[]
-            //        {
-            //            new SymmetricKeyIssuerSecurityTokenProvider(issuer, audienceSecret)
-            //        }
-            //    });
-
-            return app;
-        }
-
-
+    
         public static IUnityContainer ConfigureDependencyInjection(IUnityContainer container)
         {
             container = RegisterAllTypesIn(container, Assembly.GetExecutingAssembly());
