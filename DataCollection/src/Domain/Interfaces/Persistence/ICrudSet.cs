bc57d727b6abfa4591b73f41f5323129fecf27d9
Stephen Fuqua <stephen@safnet.com>
2016-02-26 15:22:21 -0600

    Fixed bug when loading ISurvey classes into Unity. Two interfaces extracted to files.

---
+++ DataCollection/src/Domain/Interfaces/Persistence/ICrudSet.cs
@@ -0,0 +1,11 @@
+﻿using FlightNode.Common.BaseClasses;
+using System.Data.Entity;
+
+namespace FlightNode.DataCollection.Domain.Interfaces.Persistence
+{
+    public interface ICrudSet<TEntity> : IDbSet<TEntity>
+        where TEntity : class, IEntity
+    {
+
+    }
+}
